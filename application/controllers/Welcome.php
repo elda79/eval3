065759bad22d0db122055cd03d80b09a08f66922
negrita79 <elisa.79@live.cl>
2017-10-19 20:15:02 -0300

    guardar
    crear funcion para guardar datos

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -14,6 +14,20 @@ class Welcome extends CI_Controller {
 		$this->load->view('formulario');
 	}
 	//esta funcion es la que recibe los datos
+	function recibir($data)
+	{
+		$query = $this->db->insert('usuarios', array('nombre'=>$data['elda'], 'apellido'=>$data['solar'],'Direccion de Email'=>$data['elisa'], 'Next'=>$data['listo']));
+
+		if ($this->db->affected_rows() > 0) 
+		{
+			echo "recibidos con éxito";
+		}
+		else
+		{
+			echo "Algo salio mal";
+		}
+	}
+	 // esta funcion es para guardar los datos
 	function guardar($data)
 	{
 		$query = $this->db->insert('usuarios', array('nombre'=>$data['elda'], 'apellido'=>$data['solar'],'Direccion de Email'=>$data['elisa'], 'Next'=>$data['listo']));
@@ -24,7 +38,8 @@ class Welcome extends CI_Controller {
 		}
 		else
 		{
-			echo "Algo salio mal";
+			echo "error";
 		}
 	}
+
 }
